ff42ce91183bc31935be28332aee682fd04797bf
PriyankSolanki <psolanki@condorcet93.fr>
2023-03-28 19:07:21 +0200

    creation du mockKo

---
+++ test/fr/iut/montreuil/R4_S02_2023_4/BizQuiz6_questionnaire_sme/impl/Mock/TransmettreStatsDTOMockKo.java
@@ -0,0 +1,26 @@
+package fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.impl.Mock;
+
+import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.entities.bo.QuestionnaireBO;
+import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.entities.dto.BilanStatDTO;
+import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.modeles.IServiceQuestionnaire;
+
+public class TransmettreStatsDTOMockKo implements IServiceQuestionnaire{
+
+	@Override
+	public QuestionnaireBO chargerListeQuestion(String chemin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QuestionnaireBO fournirQuestionnaire10(QuestionnaireBO q) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BilanStatDTO transmettreStat(QuestionnaireBO qbo) {
+		return null;
+	}
+
+}
